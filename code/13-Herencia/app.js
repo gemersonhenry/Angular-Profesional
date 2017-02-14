5edd1ed53c0b1ba135f0d2d2a2a287e503db9664
BZGemerson <henryingtel@gmail.com>
2017-02-13 19:37:00 -0500

    13. Explicaremos temas de herencia
    Se agregaron los archivos por defecto para poder empezar a trabajar con
    conceptos de herencia.

---
+++ code/13-Herencia/app.js
@@ -0,0 +1,14 @@
+(function () {
+
+"use strict";
+
+angular
+.module("app", [])
+.controller("HerenciaController", HerenciaController);
+
+HerenciaController.$inject = ["$scope"];
+function HerenciaController($scope) {
+
+}
+
+})();
