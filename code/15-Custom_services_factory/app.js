2ca752f508aa655c7a358ecce746fb85ddaa3218
BZGemerson <henryingtel@gmail.com>
2017-02-14 15:51:16 -0500

    15. iniciando estructura para Services Factory
    Aquí realizaremos varios ejercicios para entender el concepto de
    factory.

---
+++ code/15-Custom_services_factory/app.js
@@ -0,0 +1,14 @@
+(function () {
+
+"use strict";
+
+angular
+.module("appFactory", [])
+.controller("ShoppingController", ShoppingController);
+
+ShoppingController.$inject = [""];
+function ShoppingController() {
+	
+}
+
+})();
