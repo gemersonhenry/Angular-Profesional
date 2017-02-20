6c3797d0b1332779a9cecceaff7f73ee3e1870d3
BZGemerson <henryingtel@gmail.com>
2017-02-19 23:46:40 -0500

    Revert "Revert "18""
    This reverts commit 6f99b38d4186b740fff069956e69a9b0b7c8a8e5.

---
+++ code/18-Promesas_y_q/app.js
@@ -0,0 +1,134 @@
+(function () {
+
+"use strict";
+
+angular
+.module("appPromises", [])
+.controller("ShoppingListController", ShoppingListController)
+.service("ShoppingListService", ShoppingListService)
+.service("WeightLossFilterService", WeightLossFilterService);
+
+ShoppingListController.$inject = ['ShoppingListService'];
+function ShoppingListController (ShoppingListService) {
+	var list = this;
+
+	list.items = ShoppingListService.getItems();
+	list.itemName = "";
+	list.itemQuantity= "";
+
+	list.addItem = function () {
+		ShoppingListService.addItem(list.itemName, list.itemQuantity);
+	}
+
+	list.removeItem = function (index) {
+		ShoppingListService.removeItem(index);
+	}
+}
+
+ShoppingListService.$inject = ['$q', 'WeightLossFilterService'];
+function ShoppingListService ($q, WeightLossFilterService) {
+	var service = this;
+
+	var items = [];
+
+	// service.addItem = function (name, quantity) {
+	// 	var promise = WeightLossFilterService.checkName(name);
+	// 	promise.then(function (response) {
+	// 		var nextPromise = WeightLossFilterService.checkQuantity(quantity);
+	// 		nextPromise.then(function (result){
+	// 			var item = {
+	// 				name: name,
+	// 				quantity: quantity
+	// 			}
+	// 			items.push(item);
+	// 		}, function (errorResult){
+	// 			console.log(errorResult.message)
+	// 		});
+	// 	}, function (errorResponse){
+	// 		console.log(errorResponse.message)
+	// 	})
+	// }
+
+	// service.addItem = function (name, quantity) {
+	// 	var promise = WeightLossFilterService.checkName(name);
+
+	// 	promise
+	// 	.then(function (response) {
+	// 		return WeightLossFilterService.checkQuantity(quantity);
+	// 	})
+	// 	.then(function (response) {			
+	// 		var item = {
+	// 			name: name,
+	// 			quantity: quantity
+	// 		}
+	// 		items.push(item);			
+	// 	})
+	// 	.catch(function (errorResponse) {
+	// 		console.log(errorResponse.message);
+	// 	})
+	// }
+
+	service.addItem = function (name, quantity) {
+		var namePromise = WeightLossFilterService.checkName(name);
+		var quantityPromise = WeightLossFilterService.checkQuantity(quantity);
+		
+		$q.all([namePromise, quantityPromise])
+		.then(function (response) {			
+			var item = {
+				name: name,
+				quantity: quantity
+			}
+			items.push(item);			
+		})
+		.catch(function (errorResponse) {
+			console.log(errorResponse.message);
+		})
+	}
+
+	service.removeItem = function (index) {
+		items.splice(index,1);
+	}
+
+	service.getItems = function () {
+		return items;
+	}
+}
+
+WeightLossFilterService.$inject = ['$q', '$timeout'];
+function WeightLossFilterService ($q, $timeout) {
+	var service = this;
+
+	service.checkName = function (name) {
+		var deferred = $q.defer();
+		var result = {
+			message: ""
+		}
+		$timeout(function () {
+			if ( name.toLowerCase().indexOf('coockie')===-1 ) {
+				deferred.resolve(result);
+			} else {
+				result.message = "Stay away from cookies, Gemerson";
+				deferred.reject(result);
+			}
+		}, 3000);
+		return deferred.promise;
+	}
+
+	service.checkQuantity = function (quantity) {
+		var deferred = $q.defer();
+		var result = {
+			message: ""
+		}
+		$timeout(function () {
+			if ( quantity<6 ) {
+				deferred.resolve(result);
+			} else {
+				result.message = "That's too much, Gemerson";
+				deferred.reject(result);
+			}
+		}, 4000);
+		return deferred.promise;
+	}
+}
+
+})();
